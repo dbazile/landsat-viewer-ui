e7ee0121bead05d69c8d15915dfac2e73a87ca16
David Bazile <dbazile@users.noreply.github.com>
2020-04-04 15:33:04 -0400

    Fix property name

--- src/components/SceneList.tsx
+++ src/components/SceneList.tsx
@@ -9,7 +9,7 @@ export const SceneList = ({ className, scenes }: IProps) => (
         {scenes && scenes.features.map(scene => (
             <a key={scene.id} href={`/api/scenes/${scene.id}`} target="_blank" title="Get metadata URL" className={styles.scene}>
                 <div className={styles.scene__id}>{scene.id}</div>
-                <div className={styles.scene__timestamp}>{scene.properties.acquiredOn}</div>
+                <div className={styles.scene__timestamp}>{scene.properties.acquired}</div>
             </a>
         ))}
     </div>
